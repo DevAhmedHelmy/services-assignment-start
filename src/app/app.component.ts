11442298e758d909b404140931e7c7bdff3278d1
Rana Hisham Foad <rana@megatrust.co>
2022-02-23 11:35:11 +0200

    service assigin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'services-assignment-start';
+export class AppComponent implements OnInit {
+  activeUsers;
+  inactiveUsers;
+  constructor(private userService: UserService) {}
+  ngOnInit(): void {
+    this.activeUsers = this.userService.activeUsers;
+    this.inactiveUsers = this.userService.inactiveUsers;
+  }
 }
